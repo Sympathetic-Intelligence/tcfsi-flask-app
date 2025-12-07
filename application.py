cb14efcd74e9e92ae8dc4a8b1b4e3cc0ab0e9338
kyleiwaniec <kylehamilton@MAC-CC59DB.station>
2025-12-07 20:03:35 +0000

    remove API

--- application.py
+++ application.py
@@ -111,11 +111,11 @@ def donations():
 
 @application.route('/little_book')
 def little_book():
-    url = 'http://ipinfo.io?token=2148859d0efe1c'
-    r = requests.get(url)
-    j = json.loads(r.text)
-    country = j['country']
-
+    # url = 'http://ipinfo.io?token=2148859d0efe1c'
+    # r = requests.get(url)
+    # j = json.loads(r.text)
+    # country = j['country']
+    country="IE"
     return render_template('little_book.html', country=country)
 
 
